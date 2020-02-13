4ee83d95a8a0e39cc3e658fc9432f22f2bb7bd03
Mitchell Youngerman <MYounge@am.trimblecorp.net>
2020-02-13 15:44:14 -0500

    Worked on #2

--- Code.js
+++ Code.js
@@ -14,12 +14,20 @@ function openOptions()
       .showSidebar(html); // Display the HTML file as a sidebar.
 }
 
-function addColumn() {
+function addColumn(counter) {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var source = ss.getSheets()[1];
   var destination = ss.getSheets()[0];
-
   var range = source.getRange("F12:G15");
+  var cellForNextColumn = String.charCodeAt("L") + 3;
 
-  range.copyTo(destination.getRange("L17")); // Additional columns will start at different values.
+  if (counter == 1) // This handles the first column, which always start at cell L17.
+  {
+    range.copyTo(destination.getRange("L17")); 
+  } else { // This handles additional columns, which always start 3 cells to the right of the previous column.
+    var targetCell = String.fromCharCode(cellForNextColumn) + "17"; // Set the column and row of the current target cell.
+    range.copyTo(destination.getRange(targetCell)); 
+    cellForNextColumn = cellForNextColumn + 3; // Store the current target cell value as the previous target cell.
+  }
+  return counter;
 }
