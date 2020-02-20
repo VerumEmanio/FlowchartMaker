030d1d6449609fa63f41b14fed0909df87ecc5fe
Mitchell Youngerman <MYounge@am.trimblecorp.net>
2020-02-20 10:29:08 -0500

    Finished the basic add column feature

--- Code.js
+++ Code.js
@@ -14,20 +14,13 @@ function openOptions()
       .showSidebar(html); // Display the HTML file as a sidebar.
 }
 
-function addColumn(counter) {
+function addColumn(cellForNextColumn)
+{
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var source = ss.getSheets()[1];
   var destination = ss.getSheets()[0];
   var range = source.getRange("F12:G15");
-  var cellForNextColumn = String.charCodeAt("L") + 3; // Returns 79.
 
-  if (counter == 1) // This handles the first column, which always start at cell L17.
-  {
-    range.copyTo(destination.getRange("L17")); 
-  } else { // This handles additional columns, which always start 3 cells to the right of the previous column.
-    var targetCell = String.fromCharCode(cellForNextColumn) + "17"; // Set the column and row of the current target cell.
-    range.copyTo(destination.getRange(targetCell)); 
-    cellForNextColumn = cellForNextColumn + 3; // Store the current target cell value as the previous target cell.
-  }
-  return counter;
+  var targetCell = String.fromCharCode(cellForNextColumn) + "17"; // Set the column and row of the current target cell.
+  range.copyTo(destination.getRange(targetCell)); 
 }
